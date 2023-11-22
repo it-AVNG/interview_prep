3f43b178566a9d20508fe0cdd7d4cd0497f8f8c4
it-AVNG <it.avng@gmail.com>
2023-11-22 09:18:44 +0700

    add solution for TwoSum question

--- data_structure/Two_Sum/solution.py
+++ data_structure/Two_Sum/solution.py
@@ -1,6 +1,32 @@
-'''
+"""
 Two sum :
 assuming we have an array of number
-We are looking for pairs of number that sum up to a number int(N)
+We are looking for a pair of number that sum up to a number int(N)
+assuming that each input has exactly 1 solution
 create a python function that solve the requirement.
-'''
+"""
+
+
+def twoSum(a: list[int], num: int):
+    """return a pair of number that has the sum equal to target num"""
+
+    # create an Hashmap
+    hashMap = {}
+    # iterate through the list
+    for v, k in enumerate(a):
+        # compute the difference
+        res = num - k
+        # check the result in the list as a 'key'
+        if res in hashMap.keys():
+            # if the result is in the list, return the value of the current element and the 'value' found.
+            return (v, hashMap.get(res))
+
+        # if the result is not in the list, add the element-index as a 'key-value' pair to dict
+        hashMap.update({k: v})
+    return "Not Found"
+
+
+a = [1, 2, 5, 7, 10, 9]
+res = twoSum(a, 20)
+print(res)
+
